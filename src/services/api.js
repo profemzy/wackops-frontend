a265601f514bb0c611466d9e752bb8252bfa7b2c
Femi Oladele <profemzy@gmail.com>
2025-01-14 20:36:03 -0800

    Update API_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost/api/v1';
+const API_URL = 'https://backend.ops.infotitans.ca/api/v1';
 
 const api = axios.create({
   baseURL: API_URL,
